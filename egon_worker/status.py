4020e6d697ed448c97ab82448211db362454ec1f
Daniel <djperrefort@gmail.com>
2023-04-23 09:05:31 -0400

    Outlines classes for status reporting

--- egon_worker/status.py
+++ egon_worker/status.py
@@ -1,3 +1,4 @@
+import abc
 from enum import Enum
 
 
@@ -11,3 +12,44 @@ class Status(Enum):
     FINISHED = 'finished'
     DEGRADED = 'degraded'
     FAILED = 'failed'
+
+
+class AbstractStatusReporter(metaclass=abc.ABCMeta):
+    """Abstract cass for defining the interface of status reporting objects"""
+
+    @abc.abstractmethod
+    def report_status(self, egn_id: str, status: Status, message: str | None = '') -> None:
+        """Report the status of a Node object to the Status API"""
+
+
+class LocalStatusReporter(AbstractStatusReporter):
+    """Status reporting object used when running on a single system
+
+    For running on distributed systems, see the ``RemoteStatusReporter`` class.
+    """
+
+    def report_status(self, egn_id: str, status: Status, message: str | None = '') -> None:
+        """Report the status of a Node object to the Status API"""
+
+        raise NotImplementedError()
+
+
+class RemoteStatusReporter(AbstractStatusReporter):
+    """Status reporting object used when running on distributed systems
+
+    For running on non-distributed systems, see the ``LocalStatusReporter`` class.
+    """
+
+    def report_status(self, egn_id: str, status: Status, message: str | None = '') -> None:
+        """Report the status of a Node object to the Status API"""
+
+        raise NotImplementedError()
+
+
+class StatusReporter:
+    """Factory class returning the appropriate reporting object based on application settings"""
+
+    def __new__(cls) -> AbstractStatusReporter:
+        """Return a new status reporter instance"""
+
+        raise NotImplementedError()
